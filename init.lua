8eb6edf2bad9d4272a4825f54aacd4602619b0ae
MinetestPumpkin <114834237+MinetestPumpkin@users.noreply.github.com>
2022-10-01 21:34:34 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,4 +6,10 @@ minetest.override_item("default:river_water_source", {
   groups = {not_in_creative_inventory=1},
 })
 
+minetest.override_item("noairblocks:water_sourcex", {
+  groups = {not_in_creative_inventory=1},
+})
+
 minetest.clear_craft({output = "default:water_source"})
+minetest.clear_craft({output = "default:river_water_source"})
+minetest.clear_craft({output = "noairblocks:water_sourcex"})
