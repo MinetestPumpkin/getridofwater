b51059bb4bb7e02784d5fc7c39769c5bca5d9c9a
MinetestPumpkin <114834237+MinetestPumpkin@users.noreply.github.com>
2023-04-21 02:40:48 -0400

    Update init.lua
    re-aadd groups

--- init.lua
+++ init.lua
@@ -1,13 +1,16 @@
 minetest.override_item("default:water_source", {
-   groups = {not_in_creative_inventory=1},
+   groups = {water = 3, liquid = 3, not_in_creative_inventory = 1,
+		cools_lava = 1, not_in_creative_inventory = 1},
 })
  
 minetest.override_item("default:river_water_source", {
-  groups = {not_in_creative_inventory=1},
+  groups = {water = 3, liquid = 3, not_in_creative_inventory = 1,
+		cools_lava = 1},
 })
 
 minetest.override_item("noairblocks:water_sourcex", {
-  groups = {not_in_creative_inventory=1},
+  groups = {water = 3, liquid = 3, not_in_creative_inventory = 1,
+		cools_lava = 1},
 })
 
 minetest.clear_craft({output = "default:water_source"})
